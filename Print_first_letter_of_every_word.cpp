9ff7708d5726c55b3fbb5498ba4377390afff7d1
Apurva <70713948+Apurva-s05@users.noreply.github.com>
2021-06-07 09:49:01 +0530

    Create Print_first_letter_of_every_word.cpp
    Given a string S, the task is to create a string with the first letter of every word in the string.
     
    
    Example 1:
    
    Input: 
    S = "geeks for geeks"
    Output: gfg

---
+++ Print_first_letter_of_every_word.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	
+	string firstAlphabet(string s)
+	{
+	    for(int i=0;i<s.length();i++){
+	        if(i == 0){
+	            cout<<s[i]<<"";
+	        }if(s[i] == ' '){
+	            cout<<s[i+1]<<"";
+	        }
+	    }
+	}
+};
+
+// { Driver Code Starts.
+
+int main() 
+{
+   	
+
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	string tc;
+   	getline(cin, tc);
+   	t = stoi(tc);
+   	while(t--)
+   	{
+   		string s;
+   		getline(cin, s);
+
+   	
+        Solution ob;
+   		cout << ob.firstAlphabet(s) << "\n";
+   	}
+
+    return 0;
+} 
+  // } Driver Code Ends
